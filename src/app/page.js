38927c2956584a0f078197cceb89fe388bdaad66
asad-iqbal-76 <ai7344971@gmail.com>
2026-01-28 19:42:18 +0500

    update

--- src/app/page.js
+++ src/app/page.js
@@ -86,7 +86,47 @@ function page() {
             <p className="text-sm font-semibold">Dummy text</p>
           </div>
         </div>
+        {/* Image Gallery Section */}
+          <div className="mt-10 grid grid-cols-2 gap-6 border-t border-white pt-6">
+            <img
+              src="/jon1.jpg"
+              alt="John Wick Art 1"
+              className="w-full h-[250px] object-cover rounded-lg"
+            />
+
+            <img
+              src="/jon2.jpg"
+              alt="John Wick Art 2"
+              className="w-full h-[250px] object-cover rounded-lg"
+            />
+
+            <img
+              src="/jon3.jpg"
+              alt="John Wick Art 3"
+              className="w-full h-[250px] object-cover rounded-lg"
+            />
+
+            <img
+              src="/jon4.jpg"
+              alt="John Wick Art 4"
+              className="w-full h-[250px] object-cover rounded-lg"
+            />
+
+            <img
+              src="/jon5.jpg"
+              alt="John Wick Art 5"
+              className="w-full h-[250px] object-cover rounded-lg"
+            />
+
+            <img
+              src="/jon6.jpg"
+              alt="John Wick Art 6"
+              className="w-full h-[250px] object-cover rounded-lg"
+            />
+          </div>
+
       </div>
+      
     </div>
   );
 }
